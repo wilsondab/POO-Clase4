272ef5a681978c9e0bb210e1a811c887aaf2a84c
Alejandro Jara <alejandroejara@hotmail.com>
2024-05-07 11:14:08 -0300

    Ejercio 1 modulo 2

---
+++ EjercicioClase1Modulo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using EjercicioClase1Modulo2.Ejercicio_1;
+using EjercicioClase1Modulo2.Ejercicio_2;
+using System.Text.Json;
+
+namespace EjercicioClase1Modulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio clase 1 modulo 2");
+            // Consigna:
+            // En la ruta principal del proyecto hay 3 archivos json, para cada uno de ellos crear las clases que consideren necesarias para poder deserializar correctamente
+            // Uso: JsonSerializer.Deserialize<ClaseCreada>(data);
+
+            var pathEjercicio1 = $"{AppDomain.CurrentDomain.BaseDirectory}/Ejercicio1.json";
+            var pathEjercicio2 = $"{AppDomain.CurrentDomain.BaseDirectory}/Ejercicio2.json";
+            var pathEjercicio3 = $"{AppDomain.CurrentDomain.BaseDirectory}/Ejercicio3.json";
+
+            var dataEjercicio1 = File.ReadAllText(pathEjercicio1);
+            var dataEjercicio2 = File.ReadAllText(pathEjercicio2);
+            var dataEjercicio3 = File.ReadAllText(pathEjercicio3);
+
+
+
+
+        }
+    }
+}
